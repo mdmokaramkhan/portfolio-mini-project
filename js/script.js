bde0f2910c46a7357768d43b11b8eb1774d0d80d
mdmokaramkhan <mohammadmokaram23@lpu.in>
2026-01-30 23:10:15 +0530

    nav-scroll effect added

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var myNav = document.getElementById("mynav");
+
+window.onscroll = function () {
+    "use strict";
+    if (document.body.scrollTop >= 80 || document.documentElement.scrollTop >= 80) {
+        myNav.classList.add("nav-scrolled");
+    } else {
+        myNav.classList.remove("nav-scrolled");
+    }
+};
